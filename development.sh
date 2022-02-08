fe851388af57958dc65bd2b38d19fdabc539f25e
Indoxer <indoxer.mk@gmail.com>
2022-02-08 21:49:51 +0100

    Repair docker for windows - expose bug.

--- development.sh
+++ development.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker container stop $(docker container ls -a -q)
-docker container rm $(docker container ls -a -q)
-docker-compose down
+docker container stop $(docker container ls -a -q);
+docker container rm $(docker container ls -a -q);
+docker-compose down;
 docker-compose up --build
