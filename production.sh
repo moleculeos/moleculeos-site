3f0417b9bb62a9da3709c931ba55c98324f28cfc
Indoxer <indoxer.mk@gmail.com>
2022-02-08 18:16:58 +0100

    Change bland React to NextJs start template.

--- production.sh
+++ production.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-docker container stop $(docker container ls -a -q)
-docker container rm $(docker container ls -a -q)
-docker-compose down
 docker-compose -f docker-compose.prod.yml up --build
