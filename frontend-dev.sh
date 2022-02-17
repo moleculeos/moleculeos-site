d5350eb04dcf101cd483173f01558f851f1d932b
Indoxer <indoxer.mk@gmail.com>
2022-02-17 18:40:05 +0100

    Add development docker to nextjs.

---
+++ frontend-dev.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+docker container stop $(docker container ls -a -q);
+docker container rm $(docker container ls -a -q);
+docker-compose down;
+docker-compose -f ./frontend/moleculeos-site/docker-compose.yml --env-file .env.dev up --build
